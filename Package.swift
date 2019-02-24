82f8833711c1e6f2f9771a9f756fe22301949f01
ezura <yuka.ezura@gmail.com>
2019-02-24 18:06:30 +0900

    Add dependences

--- Package.swift
+++ Package.swift
@@ -6,15 +6,18 @@ import PackageDescription
 let package = Package(
     name: "mvvmc-template",
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "https://github.com/JohnSundell/Files.git", from: "2.2.1"),
+        .package(url: "https://github.com/JohnSundell/ShellOut.git", .branch("master"))
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "mvvmc-template",
-            dependencies: []),
+            dependencies: [
+                "Files",
+                "ShellOut"
+            ]),
         .testTarget(
             name: "mvvmc-templateTests",
             dependencies: ["mvvmc-template"]),
