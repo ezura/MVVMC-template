d4b1aa9f77a88220ce81ea5fc202a8e3f95e4ec3
ezura <yuka.ezura@gmail.com>
2019-02-24 16:45:34 +0900

    Gardening

--- Sources/mvvmc-template/Template/Template.swift
+++ Sources/mvvmc-template/Template/Template.swift
@@ -7,4 +7,41 @@
 
 import Foundation
 
-enum Template {}
+enum Template {
+    case `default`(name: String)
+
+    var model: String {
+        switch self {
+        case .default(name: let name):
+            return Template.modelTemplate(name)
+        }
+    }
+
+    var viewController: String {
+        switch self {
+        case .default(name: let name):
+            return Template.viewControllerTemplate(name)
+        }
+    }
+
+    var viewModel: String {
+        switch self {
+        case .default(name: let name):
+            return Template.viewModelTemplate(name)
+        }
+    }
+
+    var coordinator: String {
+        switch self {
+        case .default(name: let name):
+            return Template.coordinatorTemplate(name)
+        }
+    }
+
+    var coordinatorTests: String {
+        switch self {
+        case .default(name: let name):
+            return Template.coordinatorTestsTemplate(name)
+        }
+    }
+}
