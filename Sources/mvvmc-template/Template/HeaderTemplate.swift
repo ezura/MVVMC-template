7f7570f60540b37a3ebc57fcce156a1be61d6397
yuka.ezura <yuka.ezura@gmail.com>
2019-02-26 09:54:09 +0900

    Add header template

---
+++ Sources/mvvmc-template/Template/HeaderTemplate.swift
@@ -0,0 +1,21 @@
+//
+//  HeaderTemplate.swift
+//  Files
+//
+//  Created by EZURA YUKA on 2019/02/26.
+//
+
+import Foundation
+
+extension Template {
+    static let headerTemplate: String =
+        """
+        //
+        //  ___FILENAME___
+        //  ___PROJECTNAME___
+        //
+        //  Created by ___FULLUSERNAME___ on ___DATE___.
+        //  ___COPYRIGHT___
+        //
+        """
+}
