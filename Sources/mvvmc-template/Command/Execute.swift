55721df36d391a40bb10737183f1ace93100055d
yuka.ezura <yuka.ezura@gmail.com>
2019-02-26 11:56:41 +0900

    Add file header

--- Sources/mvvmc-template/Command/Execute.swift
+++ Sources/mvvmc-template/Command/Execute.swift
@@ -25,19 +25,33 @@ func execute(command: Command) throws {
         print("Creating \(rootFolderName)/\(name)")
         let folder = try rootFolder.createSubfolder(named: mainFolderName)
 
+        let username = try shellOut(to: "git config user.name")
+        let date = try shellOut(to: "date \"+%Y/%m/%d\"")
+
         try [("Model", Template.modelTemplate),
              ("ViewController", Template.viewControllerTemplate),
              ("ViewModel", Template.viewModelTemplate),
-             ("Coordinator", Template.coordinatorTemplate),
-             ("CoordinatorTests", Template.coordinatorTestsTemplate)]
+             ("Coordinator", Template.coordinatorTemplate)]
             .forEach {
+                // TODO: projectName and copyright
+                let headar = Template.headerTemplate(fileName: "\(name)\($0).swift",
+                                                     projectName: "",
+                                                     userName: username,
+                                                     date: date,
+                                                     copyright: "")
                 print("Creating \(folder.name)/\(name + $0)")
                 try folder.createFile(named: "\(name)\($0).swift",
-                                      contents: $1(name))
+                                      contents: headar + $1(name))
         }
 
         print("Creating \(rootFolder.name)/\(name)CoordinatorTests")
+        // TODO: projectName and copyright
+        let testFileHeadar = Template.headerTemplate(fileName:"\(name)CoordinatorTests.swift",
+            projectName: "",
+            userName: username,
+            date: date,
+            copyright: "")
         try rootFolder.createFile(named: "\(name)CoordinatorTests.swift",
-            contents: Template.coordinatorTestsTemplate(name))
+            contents: testFileHeadar + Template.coordinatorTestsTemplate(name))
     }
 }
--- Sources/mvvmc-template/Template/HeaderTemplate.swift
+++ Sources/mvvmc-template/Template/HeaderTemplate.swift
@@ -18,4 +18,20 @@ extension Template {
         //  ___COPYRIGHT___
         //
         """
+
+    static func headerTemplate(fileName: String, projectName: String, userName: String, date: String, copyright: String) -> String {
+        // TODO: projectName and copyright
+        let header =
+        """
+        //
+        //  \(fileName)
+        //  \(projectName)
+        //
+        //  Created by \(userName) on \(date).
+        //  \(copyright)
+        //
+        \n
+        """
+        return header
+    }
 }
