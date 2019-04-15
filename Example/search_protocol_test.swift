2d187f68984efe3e7b51092f0a019b16c501ce6d
ezura <yuka.ezura@gmail.com>
2019-04-15 21:14:49 +0900

    Update sample code of syntax tree

--- Example/search_protocol_test.swift
+++ Example/search_protocol_test.swift
@@ -7,14 +7,38 @@ protocol SampleInputs {
 
 protocol SampleOutputs {
     var buttonTitle: Driver<String> { get }
+    func f(a: Int)
 }
 
 class ViewController: SampleInputs, SampleOutputs {
+    let textDidEdit: BehaviorRelay<String?> = BehaviorRelay(value: nil)
     
     // SampleInput
     let okButtonDidTap: BehaviorRelay<Void> = PublishRelay()
-    let textDidEdit: BehaviorRelay<String?> = BehaviorRelay(value: nil)
     
     // SampleOutput
     let buttonTitle: Driver<String>
 }
+
+struct ViewController: SampleInputs, SampleOutputs where A: E {
+    let textDidEdit: BehaviorRelay<String?> = BehaviorRelay(value: nil)
+    
+    // SampleInput
+    private let okButtonDidTap : BehaviorRelay<Void> = PublishRelay()
+    
+    // SampleOutput
+    let buttonTitle: Driver<String>
+    
+    struct  Dependency {
+        let a: A
+    }
+    
+    init() {
+        print("init")
+    }
+    
+    func f(a: Int) {
+        print("")
+    }
+}
+
